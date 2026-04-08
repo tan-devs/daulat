0d4f6b04e7ef1cae4845d08007e49ec5cfa7602f
Metal Finger <dev.prajwalp@gmail.com>
2026-04-09 02:59:31 +0530

    hero

--- src/components/app/Navbar.svelte
+++ src/components/app/Navbar.svelte
@@ -1,11 +1,21 @@
-<header class="flex justify-between">
-	<div class="logo">logo</div>
-	<nav>
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+</script>
+
+<header class="flex items-center justify-between text-card-foreground px-4 pt-4">
+	<div id="logo" class="flex items-center gap-2">
+		<img src={favicon} alt="logo" class="w-8 h-8" /><span>Daulat</span>
+	</div>
+	<nav class="absolute left-1/2 -translate-x-1/2">
 		<ul class="flex gap-8">
 			<li>About us</li>
 			<li>Reviews</li>
 			<li>Blog</li>
 		</ul>
 	</nav>
-	<button>login</button>
+	<button
+		class="bg-foreground text-background text-sm font-medium p-4 rounded-2xl hover:opacity-90 transition-opacity cursor-pointer"
+	>
+		Download App
+	</button>
 </header>
